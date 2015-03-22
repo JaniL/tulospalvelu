01d03c4bbb7c0b5303ab9f05e7bdbe02d082c6f9
Jani Luukko <jj.luukko@gmail.com>
2015-03-22 23:26:05 +0200

    käyttöliittymää toteutettu alustavasti

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ set :views, Proc.new { File.join(root, "views") }
 set :public_folder, Proc.new { File.join(root, "static") }
 
 get '/' do
-  erb :layout
+  erb :index, :layout => :layout
   #"Sinatra Heroku Cedar Template - The bare minimum for a sinatra app on cedar, running rack, and using bundler."
+end
+
+get '/kisa/:id' do
+  @kisa = {}
+  @kisa['nimi'] = 'Kumpulan hiihtokisat'
+
+  erb :kisa, :layout => :layout
 end
