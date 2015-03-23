22391f14407ba377f23288c9967bb037a2fd214e
Jani Luukko <jj.luukko@gmail.com>
2015-03-23 02:35:54 +0200

    kisakäyttölittymä update

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,33 @@
 var tulospalvelu = angular.module('tulospalvelu', []);
 
-tulospalvelu.controller('TulospalveluCtrl', function ($scope) {
+tulospalvelu.controller('TulospalveluCtrl', ['$scope', '$filter', function ($scope,$filter) {
     $scope.kilpailijat = ["Matti Meikäläinen"];
     $scope.tulokset = [
         {'nimi': 'Matti Meikäläinen',
-            'aika': '02:02.22'},
+            'aika': 122022},
     ];
 
+    $scope.lahtolista = ["Matti Meikäläinen", "Jukka Kovahiihtäjä", "Dope Matti"];
+
+    $scope.aika = function(ms) {
+        var min = (ms/1000/60) << 0,
+            sec = (ms/1000) % 60;
+
+        return "0" + min + ":0" + sec.toFixed(2);
+    }
+
+    $scope.lisaaAika = function(aika) {
+        $scope.tulokset.push({
+            'nimi': aika.nimi,
+            'aika': aika.millisekunnit + (aika.sekunnit*1000) + (aika.minuutit*60*1000)
+        });
+
+        $scope.tulokset = $filter('orderBy')($scope.tulokset, 'aika');
+    }
+
+    $scope.lisaaLahtolistaan = function(kilpailija) {
+        $scope.lahtolista.splice(kilpailija.lahtosijoitus-1,0,kilpailija.nimi);
+    }
+
     $scope.orderProp = 'aika';
-});
+}]);
