85f8a9006394d581b7664a8344c3414cf95f22a0
Jani Luukko <jj.luukko@gmail.com>
2015-03-23 02:53:58 +0200

    testdata update

---
+++ sql/add_test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Kilpailija (nimi, kansallisuus, sukupuoli, syntynyt) VALUES ('Matti M', 'FI', 'M', '1992-11-11');
+INSERT INTO Kilpailija (nimi, kansallisuus, sukupuoli, syntynyt) VALUES ('Jouni A', 'FI', 'M', '1970-01-01');
+INSERT INTO Kilpailija (nimi, kansallisuus, sukupuoli, syntynyt) VALUES ('Eeva V', 'FI', 'N', '1993-05-11');
+
+INSERT INTO Kisa (nimi, alkaa, valiaikapisteita) VALUES ('Kumpulan hiihtokisat', '2014-05-05', 2);
+
+INSERT INTO KisaLahtolista (kisaId, kilpailijaId, sijoitus) VALUES (1,1,1);
+
+INSERT INTO KisaAika (kisaId,kilpailijaId,valiaikapiste,aika) VALUES (1,1,1,5000);
+INSERT INTO KisaAika (kisaId,kilpailijaId,valiaikapiste,aika) VALUES (1,1,2,15000);
